9a338100978ccbf29b0a34e3d74ba5baf6996c0a
luizfelipebraga <ruivolfb@gmail.com>
2022-03-02 19:05:07 -0300

    test: trying to preload the video

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,18 @@ import { Layout } from '../components/Layout'
 import { GlobalStyle } from '../styles/globalStyle'
 import IndexPage from './index';
 import config from 'react-reveal/globals';
+import { useEffect, useLayoutEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   config({ ssrFadeout: true });
+
+  const canUseDOM: boolean = !!(
+    typeof window !== 'undefined' &&
+    typeof window.document !== 'undefined' &&
+    typeof window.document.createElement !== 'undefined'
+  );
+  
+  const useIsomorphicLayoutEffect = canUseDOM ? useLayoutEffect : useEffect;
 
   return (
     <>

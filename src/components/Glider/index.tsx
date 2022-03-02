d91f4d91cda2a48155c0afa9ef6adb932f9ae804
luizfelipebraga <ruivolfb@gmail.com>
2022-03-02 19:20:34 -0300

    feat: need to fix preload

--- src/components/Glider/index.tsx
+++ src/components/Glider/index.tsx
@@ -12,12 +12,12 @@ export function Slider() {
   return (
     <Container>
       <Glider slidesToShow={1} hasArrows hasDots>
-        <VideoComponent video='/videos/Empresa/spain.mp4' />
         <VideoComponent video='/videos/Empresa/diligente.m4v' />
         <VideoComponent video='/videos/Empresa/logo-zoom.m4v' />
         <VideoComponent video='/videos/Empresa/RemaxPromo.m4v' />
         <VideoComponent video='/videos/Empresa/RumoAoTopoLivestream.mp4' />
         <VideoComponent video='/videos/Empresa/Slide-Conferecondominios.m4v' />
+        <VideoComponent video='/videos/Empresa/spain.mp4' />
         <VideoComponent video='/videos/BornIntro.mp4' />
         <VideoComponent video='/videos/liga-brasileira.mp4' />
       </Glider>
